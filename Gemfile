141ff2a535fe33d85bad81958c1d330da1342301
Isaac Murchie <imurchie@berkeley.edu>
2013-07-29 15:40:25 -0700

    added user model and authentication

--- Gemfile
+++ Gemfile
@@ -6,11 +6,13 @@ ruby '2.0.0'
 gem 'rails', '4.0.0'
 
 gem 'bootstrap-sass', '2.3.2.1'
+gem 'bcrypt-ruby', '3.0.0'
 
 group :development do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '1.3.7'
   gem 'rspec-rails', '2.14.0'
+  gem 'annotate', '2.5.0'
 end
 
 group :production do
@@ -21,10 +23,10 @@ end
 group :assets do
   # Use SCSS for stylesheets
   gem 'sass-rails', '~> 4.0.0'
-  
+
   # Use Uglifier as compressor for JavaScript assets
   gem 'uglifier', '>= 1.3.0'
-  
+
   # Use CoffeeScript for .js.coffee assets and views
   gem 'coffee-rails', '~> 4.0.0'
 end
---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  before_save do |user|
+    user.email.downcase!
+  end
+
+
+  validates	:name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
+                    format:   { with: /\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
