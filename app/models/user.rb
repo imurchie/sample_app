1f9734503ec65e9c3b97605254a35233fdf1db46
Isaac Murchie <imurchie@berkeley.edu>
2013-07-30 16:13:27 -0700

    finish signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ActiveRecord::Base
     user.email.downcase!
   end
 
+  before_create(:create_remember_token)
+
 
   validates	:name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
@@ -21,4 +23,18 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
