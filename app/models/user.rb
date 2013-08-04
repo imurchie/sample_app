961a29f45ee8a3463d1c9cfb1e477b86c5849372
Isaac Murchie <imurchie@berkeley.edu>
2013-08-03 22:02:37 -0700

    add user following, and compound feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@
 
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save do |user|
     user.email.downcase!
@@ -27,8 +31,22 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
+
+  def following?(user)
+    relationships.find_by(followed_id: user.id)
+  end
+
+  def follow!(user)
+    relationships.create!(followed_id: user.id)
+  end
+
+  def unfollow!(user)
+    relationships.find_by(followed_id: user.id).destroy!
+  end
+
+
 
 
   def self.new_remember_token
